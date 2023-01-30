0270315de6de5de44f25ac792b11f200170e57b9
unknown <notquotemark@gmail.com>
2023-01-29 20:17:58 -0800

    editor colors

--- Hayase Renderer/Editor/Editor.cpp
+++ Hayase Renderer/Editor/Editor.cpp
@@ -39,18 +39,13 @@ namespace Hayase
 
 		ImGui::StyleColorsDark();
 
-		ImGuiStyle& style = ImGui::GetStyle();
-		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-		{
-			style.WindowRounding = 0.0f;
-			style.Colors[ImGuiCol_WindowBg].w = 1.0f;
-		}
-
 		Application& app = Application::Get();
 		GLFWwindow* window = static_cast<GLFWwindow*>(app.GetWindow().GetWindow());
 
 		ImGui_ImplGlfw_InitForOpenGL(window, true);
 		ImGui_ImplOpenGL3_Init("#version 430");
+
+		CustomizeColors();
 
 		int w = app.GetWindow().GetWidth();
 		int h = app.GetWindow().GetHeight();
@@ -209,6 +204,41 @@ namespace Hayase
 
 	void Editor::CustomizeColors()
 	{
-	
+		ImGui::GetStyle().WindowRounding = 0.0f;
+
+		auto& colors = ImGui::GetStyle().Colors;
+
+		colors[ImGuiCol_Text] = ImVec4{ 0.227f, 0.667f, 0.98f, 1.0f };
+		colors[ImGuiCol_WindowBg] = ImVec4{ 0.078f, 0.074f, 0.087f, 1.0f };
+
+		// Headers
+		colors[ImGuiCol_Header] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+		colors[ImGuiCol_HeaderHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
+		colors[ImGuiCol_HeaderActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+
+		// Buttons
+		colors[ImGuiCol_Button] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+		colors[ImGuiCol_ButtonHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
+		colors[ImGuiCol_ButtonActive] = ImVec4{ 0.22f, 0.244f, 0.26f, 1.0f };
+
+		// Frame BG
+		colors[ImGuiCol_FrameBg] = ImVec4{ 0.189f, 0.184f, 0.212f, 1.0f };
+		colors[ImGuiCol_FrameBgHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
+		colors[ImGuiCol_FrameBgActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+
+		// Tabs
+		colors[ImGuiCol_Tab] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_TabHovered] = ImVec4{ 0.38f, 0.3805f, 0.381f, 1.0f };
+		colors[ImGuiCol_TabActive] = ImVec4{ 0.28f, 0.2805f, 0.281f, 1.0f };
+		colors[ImGuiCol_TabUnfocused] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_TabUnfocusedActive] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+
+		// Title
+		colors[ImGuiCol_TitleBg] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_TitleBgActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_TitleBgCollapsed] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+
+		colors[ImGuiCol_SliderGrab] = ImVec4{ 0.392f, 0.361f, 0.498f, 1.0f };
+		colors[ImGuiCol_SliderGrabActive] = ImVec4{ 0.492f, 0.461f, 0.598f, 1.0f };
 	}
 }
